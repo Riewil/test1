ff9b8268430feefc84382c3601e17cbfa3e59234
maksim <larkin2003@inbox.ru>
2022-04-06 22:10:22 +0400

    gitIgnore

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] massiv = new Int32[9] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            for (int numberOne = 0; numberOne < 9; numberOne++)
+            {
+                for (int numberTwo = 0; numberTwo < 9; numberTwo++)
+                {
+                    Console.WriteLine($"{massiv[numberOne]} + {massiv[numberTwo]} = {massiv[numberOne] + massiv[numberTwo]}");
+                }
+            }
+        }
+    }
+}
