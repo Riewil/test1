88a63819636e3eb8de37ce8c1891a6c1c9577a88
maksim <larkin2003@inbox.ru>
2022-04-06 22:39:04 +0400

    Исправил ошибки

--- WorkWithArray/AdditionTable.cs
+++ WorkWithArray/AdditionTable.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-
-namespace ConsoleApplication1
+namespace WorkWithArray
 {
-    internal class Program
+    internal class AdditionTable
     {
         public static void Main(string[] args)
         {
-            int[] massiv = new Int32[9] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int[] numbersArray = new Int32[9] {1, 2, 3, 4, 5, 6, 7, 8, 9};
             for (int numberOne = 0; numberOne < 9; numberOne++)
             {
                 for (int numberTwo = 0; numberTwo < 9; numberTwo++)
                 {
-                    Console.WriteLine($"{massiv[numberOne]} + {massiv[numberTwo]} = {massiv[numberOne] + massiv[numberTwo]}");
+                    Console.WriteLine($"{numbersArray[numberOne]} + {numbersArray[numberTwo]} = {numbersArray[numberOne] + numbersArray[numberTwo]}");
                 }
             }
         }
